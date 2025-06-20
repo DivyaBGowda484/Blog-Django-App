469b29b8de36287733c4d15712dcce62b020a0e4
Divya B Gowda <117468949+DivyaBGowda484@users.noreply.github.com>
2025-06-21 00:18:50 +0530

    Django Blog App with MySQL and Bootstrap

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# For development
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
